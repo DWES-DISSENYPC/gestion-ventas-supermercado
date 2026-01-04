90d80e3d0348e78cf99ac04de782751970ae8162
DissenyPc <mekwen10@hotmail.es>
2026-01-04 11:39:30 +0100

    A falta de depurar

--- supermercado/src/main/java/com/example/supermercado/Controller/VentaController.java
+++ supermercado/src/main/java/com/example/supermercado/Controller/VentaController.java
@@ -22,11 +22,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/ventas")
+@SessionAttributes("v")
 public class VentaController {
 
     private VentaServices vs;
